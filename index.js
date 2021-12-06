08ec69a01286ccaf922ede9a3ad1975bb7b918ac
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-06 07:54:45 -0500

    Add chrismassified class on btn click

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const christmassifierBtn = document.getElementById('christmassifierBtn')
 christmassifierBtn.addEventListener('click', christmassifyName)
 
 function christmassifyName() {
-  // Task:
-  // - Add christmassify class to greeting.
-  //- Check whether christmassifierBtn has christmassify on it. If so, change text to "De-christmassify", if not, change text to "Christmassify"
+  greeting.className = 'christmassified'
 }
+
+// Task:
+// - Add christmassify class to greeting.
+//- Check whether christmassifierBtn has christmassify on it. If so, change text to "De-christmassify", if not, change text to "Christmassify"
 
 // Stretch goals:
 // - Play Christmas music when Christmas class is added.
