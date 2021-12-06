2d2cb46a5bb179b224729dc429e85251fc7816c4
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-06 08:08:12 -0500

    Add awesome xmas music

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const greeting = document.getElementById('greeting')
 const christmassifierBtn = document.getElementById('christmassifierBtn')
 christmassifierBtn.addEventListener('click', christmassifyName)
 
+const xmasMusic = new Audio('./fire-and-ice.mp3')
+xmasMusic.volume = 0.4
+
 function christmassifyName() {
   if (greeting.className === 'christmassified') {
     greeting.className = ''
     christmassifierBtn.textContent = 'Christmassify'
+    xmasMusic.pause()
   } else {
     greeting.className = 'christmassified'
     christmassifierBtn.textContent = 'De-christmassify'
+    xmasMusic.play()
   }
 }
 
